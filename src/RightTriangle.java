c0ca7b7d9254d7e76c3f1255b921e1387b79a5ed
saltanat <saltanatnematillakyzy6@gmail.com>
2022-11-16 14:41:43 +0600

    Initial commit

---
+++ src/RightTriangle.java
@@ -0,0 +1,52 @@
+public class RightTrangle extends Shape{
+    private int a;
+    private int b;
+    private int c;
+
+    public RightTrangle(String name, int a, int b, int c) {
+        super(name);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    @Override
+    public double getPerimeter() {
+        int p = a + b + c;
+        System.out.println("right trangle: "+ p);
+        return p;
+    }
+
+    @Override
+    public String toString() {
+        return "RightTrangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,53 @@
+public class Trangle extends Shape {
+    private int a;
+    private int b;
+    private int c;
+
+
+    public Trangle(String name, int a, int b, int c) {
+        super(name);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    @Override
+    public double getPerimeter() {
+        int p = a + b + c;
+        System.out.println("Tangle perimeter: "+ p);
+        return p;
+    }
+
+    @Override
+    public String toString() {
+        return "Trangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
+    }
+}
